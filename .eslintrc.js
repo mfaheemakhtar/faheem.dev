6311d3b978fdb1aa4f71913efab5666fb72e0299
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2019-12-19 10:38:53 +0200

    Update eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,25 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
+  extends: ['plugin:react/recommended', 'airbnb'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'sort-keys': 1,
+    'sort-vars': 1,
+    'import/named': 0,
+    'no-underscore-dangle': 0,
   },
-}
+};
