6ba85586c6b1602e077265078b3287223da07784
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2019-12-25 21:03:02 +0500

    Remove footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,10 +15,6 @@ function Layout(props) {
     return (
       <div>
         <main>{children}</main>
-        <footer>
-          ©{new Date().getFullYear()}, Built with{' '}
-          <a href="https://www.gatsbyjs.org"> Gatsby</a>
-        </footer>
       </div>
     );
   }
