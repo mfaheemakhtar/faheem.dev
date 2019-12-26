0bbfb7efb495effe9e05d0c99cda10381285e309
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2019-12-26 19:20:37 +0500

    Update button component to add link feature

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,12 +3,34 @@ import React from 'react';
 import './styles.scss';
 
 function Button(props) {
-  const { children, className, autocomplete, disabled, type } = props;
+  const {
+    autocomplete,
+    children,
+    className,
+    disabled,
+    href,
+    rel,
+    target,
+    type,
+  } = props;
+
+  if (type === 'link') {
+    return (
+      <a
+        className={`button button-link ${className}`}
+        href={href}
+        rel={rel}
+        target={target}
+      >
+        {children}
+      </a>
+    );
+  }
 
   return (
     <button
       autoComplete={autocomplete}
-      className={`Button ${className}`}
+      className={`button ${className}`}
       disabled={disabled}
       type={type}
     >
@@ -21,14 +43,20 @@ Button.defaultProps = {
   autocomplete: 'off',
   className: '',
   disabled: false,
+  href: '#',
+  rel: 'noreferrer noopener',
+  target: '_blank',
   type: 'button',
 };
 
 Button.propTypes = {
   autocomplete: PropTypes.string,
-  children: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
   className: PropTypes.string,
   disabled: PropTypes.bool,
+  href: PropTypes.string,
+  rel: PropTypes.string,
+  target: PropTypes.string,
   type: PropTypes.string,
 };
 
