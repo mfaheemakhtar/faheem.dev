401a18593082046488c3444d149a256ce371852c
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2020-01-02 17:15:45 +0500

    Add figure to Image component

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -3,22 +3,28 @@ import React from 'react';
 import './styles.scss';
 
 function Image(props) {
-  const { alt, className, source, title } = props;
+  const { alt, caption, className, source, title } = props;
 
   return (
-    <picture className={`image ${className}`}>
-      <source srcSet={source} />
-      <img alt={alt} src={source} title={title} />
-    </picture>
+    <figure className={`image ${className}`}>
+      <picture>
+        <source srcSet={source} />
+        <img alt={alt} src={source} title={title} />
+      </picture>
+
+      {caption && <figcaption className="caption">{caption}</figcaption>}
+    </figure>
   );
 }
 
 Image.defaultProps = {
+  caption: '',
   className: '',
 };
 
 Image.propTypes = {
   alt: PropTypes.string.isRequired,
+  caption: PropTypes.string,
   className: PropTypes.string,
   source: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
