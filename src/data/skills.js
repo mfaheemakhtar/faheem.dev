8aa4cecc2e46f14b45bf2ae1e73da12f174390be
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2020-01-02 17:17:08 +0500

    Update Skills section

---
+++ src/data/skills.js
@@ -0,0 +1,123 @@
+export const SKILLS_EXPANDED = [
+  {
+    items: [
+      {
+        alt: 'JavaScript',
+        caption: 'JavaScript',
+        fileName: 'javascript',
+      },
+      {
+        alt: 'NodeJS',
+        caption: 'NodeJS',
+        fileName: 'nodejs',
+      },
+      {
+        alt: 'ReactJS',
+        caption: 'ReactJS',
+        fileName: 'reactjs',
+      },
+    ],
+    title: 'Primary Stack',
+  },
+
+  {
+    items: [
+      {
+        alt: 'Material UI',
+        caption: 'Material UI',
+        fileName: 'material-ui',
+      },
+      {
+        alt: 'Reactstrap',
+        caption: 'Reactstrap',
+        fileName: 'reactstrap',
+      },
+      {
+        alt: 'Ant Design',
+        caption: 'Ant Design',
+        fileName: 'ant-design',
+      },
+      {
+        alt: 'Bootstrap',
+        caption: 'Bootstrap',
+        fileName: 'bootstrap',
+      },
+    ],
+    title: 'UI Components Libraries/Frameworks',
+  },
+
+  {
+    items: [
+      {
+        alt: 'MongoDB',
+        caption: 'MongoDB',
+        fileName: 'mongodb',
+      },
+      {
+        alt: 'MySQL',
+        caption: 'MySQL',
+        fileName: 'mysql',
+      },
+    ],
+    title: 'Databases',
+  },
+
+  {
+    items: [
+      {
+        alt: 'AWS',
+        caption: 'AWS',
+        fileName: 'aws',
+      },
+      {
+        alt: 'Firebase',
+        caption: 'Firebase',
+        fileName: 'firebase',
+      },
+      {
+        alt: 'Heroku',
+        caption: 'Heroku',
+        fileName: 'heroku',
+      },
+      {
+        alt: 'Digital Ocean',
+        caption: 'Digital Ocean',
+        fileName: 'digital-ocean',
+      },
+    ],
+    title: 'Cloud Services',
+  },
+];
+
+export const SKILLS_PRIMARY = [
+  {
+    items: [
+      {
+        alt: 'NodeJS',
+        caption: 'NodeJS',
+        fileName: 'nodejs',
+      },
+      {
+        alt: 'ReactJS',
+        caption: 'ReactJS',
+        fileName: 'reactjs',
+      },
+      {
+        alt: 'JavaScript',
+        caption: 'JavaScript',
+        fileName: 'javascript',
+      },
+      {
+        alt: 'MongoDB',
+        caption: 'MongoDB',
+        fileName: 'mongodb',
+      },
+      {
+        alt: 'AWS',
+        caption: 'AWS',
+        fileName: 'aws',
+      },
+    ],
+    title: 'Primary Stack',
+  },
+];
